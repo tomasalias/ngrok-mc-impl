235ae84ed3057143a46d4f973716982f8579561d
Dominik <80042910+tomasalias@users.noreply.github.com>
2023-12-27 00:03:17 +0100

    Update NgrokCommunication.java

--- src/main/java/me/infinity/ngrokcom/NgrokCommunication.java
+++ src/main/java/me/infinity/ngrokcom/NgrokCommunication.java
@@ -7,10 +7,18 @@ import com.github.alexdlaird.ngrok.protocol.Proto;
 import com.github.alexdlaird.ngrok.protocol.Region;
 import com.github.alexdlaird.ngrok.protocol.Tunnel;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.InetAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.stream.Collectors;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import discord4j.common.util.Snowflake;
 import discord4j.core.DiscordClientBuilder;
@@ -99,85 +107,104 @@ public final class NgrokCommunication extends JavaPlugin {
     }
     
     private void updateDynuDNS() {
-        try {
-            // Grab Full Ngrok Address
-            String ngrokAddress = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/ngrok.log")))
-                    .lines().findFirst().orElse("");
-            ngrokAddress = ngrokAddress.substring(ngrokAddress.indexOf("tcp://") + 6, ngrokAddress.indexOf(" "));
+        if (System.getenv("DYNU_CLIENT_ID") != null) {
+            try {
+                // Wait for 10 seconds
+                Thread.sleep(10000);
 
-            // Grab Ngrok IP and Port
-            String ngrokIp = ngrokAddress.split(":")[0];
-            String ngrokPort = ngrokAddress.split(":")[1];
+                // Grab Full Ngrok Address
+                String ngrokAddress = Files.readString(Paths.get("ngrok.log"));
+                ngrokAddress = ngrokAddress.split("tcp://")[1].split(" ")[0];
 
-            // Retrieve Dynu Keys
-            String dynuClientId = getConfig().getString("DYNU_API.CLIENT_ID");
-            String dynuSecret = getConfig().getString("DYNU_API.SECRET");
-            String dynuFull = dynuClientId + ":" + dynuSecret;
+                // Grab Ngrok IP and Port
+                String ngrokIp = ngrokAddress.split(":")[0];
+                ngrokIp = InetAddress.getByName(ngrokIp).getHostAddress();
+                int ngrokPort = Integer.parseInt(ngrokAddress.split(":")[1]);
 
-            // Retrieve Dynu Token
-            URL tokenUrl = new URL("https://api.dynu.com/v2/oauth2/token");
-            HttpURLConnection tokenConnection = (HttpURLConnection) tokenUrl.openConnection();
-            tokenConnection.setRequestMethod("GET");
-            tokenConnection.setRequestProperty("Authorization", "Basic " + dynuFull);
-            tokenConnection.setRequestProperty("Accept", "application/json");
+                // Retrieve Dynu Keys
+                String dynuClientId = System.getenv("DYNU_CLIENT_ID");
+                String dynuSecret = System.getenv("DYNU_SECRET");
+                String dynuFull = dynuClientId + ":" + dynuSecret;
 
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(tokenConnection.getInputStream()))) {
-                String response = reader.lines().reduce("", String::concat);
-                String dynuToken = response.split("\"")[3];
+                // Retrieve Dynu Token
+                String dynuToken = curlGetRequest("https://api.dynu.com/v2/oauth2/token",
+                        "accept: application/json",
+                        "-u", dynuFull);
+                dynuToken = dynuToken.split("\"")[3];
 
                 // Get Domain Name + ID
-                URL domainUrl = new URL("https://api.dynu.com/v2/dns");
-                HttpURLConnection domainConnection = (HttpURLConnection) domainUrl.openConnection();
-                domainConnection.setRequestMethod("GET");
-                domainConnection.setRequestProperty("Authorization", "Bearer " + dynuToken);
-                domainConnection.setRequestProperty("Accept", "application/json");
+                String domain = curlGetRequest("https://api.dynu.com/v2/dns",
+                        "accept: application/json",
+                        "Authorization: Bearer " + dynuToken);
+                String domainId = domain.split("\"")[6];
+                domainId = domainId.substring(1, domainId.length() - 1);
+                String domainName = domain.split("\"")[9];
 
-                try (BufferedReader domainReader = new BufferedReader(new InputStreamReader(domainConnection.getInputStream()))) {
-                    String domainResponse = domainReader.lines().reduce("", String::concat);
-                    String domainId = domainResponse.split("\"")[6].replace("\"", "");
+                // Get DNS Records of Domain ID
+                String dnsRecords = curlGetRequest("https://api.dynu.com/v2/dns/" + domainId + "/record",
+                        "accept: application/json",
+                        "Authorization: Bearer " + dynuToken);
+                String dnsesIp = dnsRecords.split("id")[2].split(",")[0].substring(3);
+                String dnsesPort = dnsRecords.split("id")[3].split(",")[0].substring(3);
 
-                    // Get DNS Records of Domain ID
-                    URL dnsUrl = new URL("https://api.dynu.com/v2/dns/" + domainId + "/record");
-                    HttpURLConnection dnsConnection = (HttpURLConnection) dnsUrl.openConnection();
-                    dnsConnection.setRequestMethod("GET");
-                    dnsConnection.setRequestProperty("Authorization", "Bearer " + dynuToken);
-                    dnsConnection.setRequestProperty("Accept", "application/json");
+                // Update DNS for Ngrok IP
+                String updateIpResponse = curlPostRequest("https://api.dynu.com/v2/dns/" + domainId + "/record/" + dnsesIp,
+                        "accept: application/json",
+                        "Authorization: Bearer " + dynuToken,
+                        "Content-Type: application/json",
+                        "{\"nodeName\":\"mcngrok\",\"recordType\":\"A\",\"ttl\":120,\"state\":true,\"group\":\"\",\"ipv4Address\":\"" + ngrokIp + "\"}");
 
-                    try (BufferedReader dnsReader = new BufferedReader(new InputStreamReader(dnsConnection.getInputStream()))) {
-                        String dnsResponse = dnsReader.lines().reduce("", String::concat);
-                        String[] dnsRecords = dnsResponse.split("\"id\"");
+                getLogger().info("Update IP Response: " + updateIpResponse);
 
-                        String dnsIpId = dnsRecords[1].split(",")[0].replace(":", "").replace("\"", "");
-                        String dnsPortId = dnsRecords[2].split(",")[0].replace(":", "").replace("\"", "");
+                // Update DNS for Ngrok PORT
+                String updatePortResponse = curlPostRequest("https://api.dynu.com/v2/dns/" + domainId + "/record/" + dnsesPort,
+                        "accept: application/json",
+                        "Authorization: Bearer " + dynuToken,
+                        "Content-Type: application/json",
+                        "{\"nodeName\":\"_minecraft._tcp\",\"recordType\":\"SRV\",\"ttl\":120,\"state\":true,\"group\":\"\",\"host\":\"mcngrok." + domainName + "\",\"priority\":10,\"weight\":5,\"port\":" + ngrokPort + "}");
 
-                        // Update DNS for Ngrok IP
-                        updateDynuRecord(domainId, dnsIpId, ngrokIp, "A");
+                getLogger().info("Update Port Response: " + updatePortResponse);
 
-                        // Update DNS for Ngrok Port
-                        updateDynuRecord(domainId, dnsPortId, ngrokPort, "SRV");
-                    }
-                }
+                getLogger().info("Finished DNS Update!\nConnect to your Server Now at '" + domainName + "'");
+            } catch (IOException | InterruptedException e) {
+                getLogger().log(Level.SEVERE, "Error updating Dynu DNS", e);
             }
-        } catch (Exception e) {
-            getLogger().severe("Error updating Dynu DNS: " + e.getMessage());
         }
     }
 
-    private void updateDynuRecord(String domainId, String recordId, String value, String recordType) throws Exception {
-        String apiUrl = "https://api.dynu.com/v2/dns/" + domainId + "/record/" + recordId;
-        URL url = new URL(apiUrl);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Authorization", "Bearer " + getConfig().getString("DYNU_API.TOKEN"));
-        connection.setRequestProperty("Accept", "application/json");
-        connection.setRequestProperty("Content-Type", "application/json");
-        connection.setDoOutput(true);
+ // Helper method to perform a GET request using curl
+    private String curlGetRequest(String url, String... headers) throws IOException {
+        List<String> command = new ArrayList<>();
+        command.add("curl");
+        command.add("--silent");
+        command.add("-X");
+        command.add("GET");
+        command.add(url);
+        command.addAll(Arrays.asList(headers));
 
-        String jsonBody = "{\"nodeName\":\"mcngrok\",\"recordType\":\"" + recordType + "\",\"ttl\":120,\"state\":true,\"group\":\"\",\"ipv4Address\":\"" + value + "\"}";
-        connection.getOutputStream().write(jsonBody.getBytes());
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+        Process process = processBuilder.start();
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-            // Handle response if needed
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
+    }
+
+    // Helper method to perform a POST request using curl
+    private String curlPostRequest(String url, String... headersAndData) throws IOException {
+        List<String> command = new ArrayList<>();
+        command.add("curl");
+        command.add("--silent");
+        command.add("-X");
+        command.add("POST");
+        command.add(url);
+        command.addAll(Arrays.asList(headersAndData));
+
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+        Process process = processBuilder.start();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            return reader.lines().collect(Collectors.joining("\n"));
         }
     }
 
@@ -199,22 +226,13 @@ public final class NgrokCommunication extends JavaPlugin {
         }
 
     }
-    // Method to save the configuration without generating warnings
+
+    // Method to save the configuration
     private void saveConfigSilently() {
         try {
-            // Temporarily store the original system output and error streams
-            // to suppress warnings during configuration save
-            System.setOut(null);
-            System.setErr(null);
-
-            // Save the default configuration
             saveDefaultConfig();
-
-            // Restore the original output and error streams
-            System.setOut(System.out);
-            System.setErr(System.err);
         } catch (Exception e) {
-            e.printStackTrace();
+        	            getLogger().log(Level.SEVERE, "Error saving configuration", e);
         }
     }
 }
